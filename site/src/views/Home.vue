5167dc66cfae206e9912627d85131ffe45ee8a4c
New FronTier Works <nftw@tuta.io>
2022-02-16 10:19:05 -0700

    standardize names and setup validator

--- site/src/views/Home.vue
+++ site/src/views/Home.vue
@@ -1,15 +1,20 @@
 <template>
-  <qr-generator />
+  <v-container>
+    <qr-generator />
+    <qr-validator />
+  </v-container>
 </template>
 
 <script>
   import QrGenerator from '../components/QrGenerator.vue'
+  import QrValidator from '../components/QrValidator.vue'
 
   export default {
     name: 'Home',
 
     components: {
       QrGenerator,
+      QrValidator
     },
   }
 </script>
