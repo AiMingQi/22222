81e7fd493d916d6fa1992e8a4a0ded00d9f304d1
New FronTier Works <nftw@tuta.io>
2022-02-24 14:07:24 -0700

    add LIT Protocol and component

---
+++ site/src/components/ConnectLitNetwork.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col class="mb-4">
+        <v-img id="canvas" :src="url" max-width="300px" class="mx-auto"></v-img>        
+      </v-col>      
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+
+import LitJsSdk from 'lit-js.sdk'
+ 
+  export default {
+    name: 'ConnectLitNetwork',
+    data: () => ({
+    }),
+    created(){
+      // With promises
+        const client = new LitJsSdk.LitNodeClient()
+        await client.connect()
+        window.litNodeClient = client
+    },
+    methods: {
+     
+    },
+  }
+</script>
