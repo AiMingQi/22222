fd195de3dbfe4982fd4b53756206aeae2cadf859
New FronTier Works <nftw@tuta.io>
2022-03-04 20:45:04 -0700

    almost got a JWT

--- site/src/components/ConnectLitNetwork.vue
+++ site/src/components/ConnectLitNetwork.vue
@@ -2,7 +2,44 @@
   <v-container>
     <v-row class="text-center">
       <v-col class="mb-4">
-        <v-img id="canvas" :src="url" max-width="300px" class="mx-auto"></v-img>        
+      <v-card>
+        <h2>Get Lit</h2>    
+        {{txHash}}
+        <br>
+        {{chain}}
+        <br>
+        {{provisioningStatus}}
+        <br>
+        {{requestingStatus}}
+        <br>
+       TokenId: {{tokenId}}
+        <br>
+        Token Address: {{tokenAddress}}
+        <br>
+        authSig: {{authSig}}
+        <br>
+        ResourceId {{resourceId}}
+        <br>
+        Access Control {{accessControlConditions}}
+        <br>
+        JWT {{jwt}}
+        <br>
+        <v-btn @click="getLit" block dark>
+          Get Lit
+        </v-btn>
+        <v-btn @click="mintNft" block>
+          Mint Token
+        </v-btn>
+        <v-btn @click="provisionAccess" block dark>
+          Provision Access
+        </v-btn>
+        <v-btn @click="requestJwt" block>
+          Request JWT
+        </v-btn>
+        <v-btn @click="verifyJwt" block dark>
+          Verify JWT
+        </v-btn>
+      </v-card>
       </v-col>      
     </v-row>
   </v-container>
@@ -10,20 +47,102 @@
 
 <script>
 
-import LitJsSdk from 'lit-js.sdk'
+import LitJsSdk from 'lit-js-sdk'
  
   export default {
     name: 'ConnectLitNetwork',
     data: () => ({
+      txHash: 'no txHash',
+      chain: 'rinkeby',
+      provisioningStatus: 'not provisioning',
+      requestingStatus: 'not requesting',
+      mintingAddress: 'no minting address',
+      listNodeClient: {},
+      authSig: 'no authSig',
+      tokenId: 'no tokenID',
+      tokenAddress: 'no token Address',
+      knowntokenID: '0x54257a1b2cccd749f896673640f87d9856764df0614fb0483a898258e27a85d5',
+      accessControlConditions: [],
+      resourceId: {},
+      jwt: {},     
+
     }),
     created(){
       // With promises
-        const client = new LitJsSdk.LitNodeClient()
-        await client.connect()
-        window.litNodeClient = client
+        LitJsSdk.litJsSdkLoadedInALIT()
+        this.getLit()
     },
     methods: {
-     
+      async getLit () {
+        const client = new LitJsSdk.LitNodeClient()
+        await client.connect()
+        this.litNodeClient = client
+        this.accessControlConditions = [
+          {
+            contractAddress: LitJsSdk.LIT_CHAINS[this.chain].contractAddress,
+            standardContractType: 'ERC1155',
+            chain: 'rinkeby',
+            method: 'balanceOf',
+            parameters: [
+              ':userAddress',
+              this.tokenId.toString()
+            ],
+            returnValueTest: {
+              comparator: '>',
+              value: '0'
+            }
+          }
+        ];
+        this.resourceId = {
+          baseUrl: 'localhost:8080/#/',
+          path: 'red', // this would normally be your url path, like "/webpage.html" for example
+          orgId: "",
+          role: "",
+          extraData: ""
+        }
+      },
+      async mintNft () {
+          const {
+          txHash,
+          tokenId,
+          tokenAddress,
+          mintingAddress,
+          authSig
+        } = await LitJsSdk.mintLIT({ chain: this.chain, quantity: 1 })
+        this.tokenId = tokenId
+        this.tokenAddress = tokenAddress
+        this.authSig = authSig
+        this.txHash = txHash
+        this.mintingAddress = mintingAddress
+      },
+
+      async provisionAccess () {
+        this.provisioningStatus = 'Provisioning, please wait...'
+        console.log(this.accessControlConditions)
+
+        await this.litNodeClient.saveSigningCondition({
+          accessControlConditions: this.accessControlConditions,
+          chain: this.chain,
+          authSig: this.authSig,
+          resourceId: this.resourceId
+        })
+
+        this.provisioningStatus = 'Provisioned!'
+      },
+      async requestJwt () {
+        this.requestingStatus = 'Requesting JWT, please wait...'
+        this.jwt = await this.litNodeClient.getSignedToken({
+          accessControlConditions: this.accessControlConditions,
+          chain: this.chain,
+          authSig: this.authSig,
+          resourceId: this.resourceId
+        }) 
+
+        this.requestingStatus = 'JWT obtained, please wait...' + this.jwt
+      },
+      async verifyJwt () {
+
+      }
     },
   }
 </script>
