50f351c13891ee7298265d01450e0aae9d51e0b5
New FronTier Works <nftw@tuta.io>
2022-03-20 16:24:14 -0600

    integrate firebase

--- site/src/views/UserCounter.vue
+++ site/src/views/UserCounter.vue
@@ -4,6 +4,7 @@
             <v-col>
                 <v-card class="pa-2"> 
                     <h2>User Counter</h2>
+                    <v-btn @click="getFirebase">Get Firebase</v-btn>
                     <tableland/>
                 </v-card> 
             </v-col>
@@ -13,6 +14,7 @@
 
 <script>
   import Tableland from '../components/Tableland.vue'
+  import { initializeApp } from "firebase/app";
 
   export default {
     name: 'UserCounter',
@@ -20,5 +22,19 @@
     components: {
       Tableland,
     },
+    methods: {
+      getFirebase () {
+        const firebaseConfig = {
+          apiKey: "AIzaSyCNihpF5XcCatwd62b-zUkI-IWlw_mQC3Q",
+          authDomain: "world-bd12b.firebaseapp.com",
+          projectId: "world-bd12b",
+          storageBucket: "world-bd12b.appspot.com",
+          messagingSenderId: "906014027852",
+          appId: "1:906014027852:web:9a8af9e857eacb799a44e9"
+        };
+        const app = initializeApp(firebaseConfig);
+        console.log(app)
+      }
+    }
   }
 </script>
